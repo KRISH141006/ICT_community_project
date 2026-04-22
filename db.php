909888439db251879c65d535b51be19e2f9330d3
krish sondagar <krishsondagar13@gmail.com>
2026-04-22 14:14:49 +0530

    sharing com's ip for db

--- db.php
+++ db.php
@@ -1,11 +1,12 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "Taksh@07";
+$host = "10.129.151.226";
+$user = "krish";
+$pass = "";
 $db   = "ICT_Community";
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+// echo "connected";
 ?>
